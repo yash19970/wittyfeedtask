bc5db18c7bb45fab97087039931cb536438513a7
yash19970 <yash_19@outlook.com>
2016-12-23 19:06:16 +0530

    crud operations done

--- FlaskMongo/app.py
+++ FlaskMongo/app.py
@@ -1,27 +1,33 @@
-from flask import Flask, render_template 
+from flask import Flask, render_template, url_for, request , session, redirect 
 from flask.ext.pymongo import PyMongo
 app = Flask(__name__)
 app.config['MONGO_DBNAME'] = 'EmpData' 
 app.config['MONGO_URI'] = 'mongodb://localhost/EmpData'
 mongo = PyMongo(app)
 
+@app.route('/dashboard')
+def dashboard():
+	users = mongo.db.users
+	result = users.find()
+	print result
+	return render_template('show.html',result=result)
 
-@app.route('/')
-def index():
-	return render_template('login.html') 
+@app.route('/insertdata', methods = ['POST','GET'])
+def insertdata():
+	if request.method == 'POST':
+		users = mongo.db.users
+		result = users.insert({'org':request.form['org'], 'name' : request.form['name']})
+		return redirect(url_for('insertdata'))
+	return render_template('dashboard.html')
 
-@app.route('/find')
-def find():
-	user = mongo.db.users
-	result = user.find_one({'name':'yash'})
-	#return render_template(,'show.html')
-	return result['name']+result['org'] 
-
-@app.route('/add')
-def add():
-	user = mongo.db.users
-	user.insert({'name' : 'ritik', 'org'  : 'wittyfeed'})
-	user.insert({'name' : 'sumit', 'org'  : 'viral9'})
-	return	'Added user!'
+ 
+@app.route('/deleteData', methods = ['POST','GET'])
+def deleteData():
+	if request.method == 'POST':
+		condition = request.form['name']
+		users = mongo.db.users
+		result = users.delete_many({'name': request.form['name']})
+		return redirect(url_for('deleteData'))
+	return render_template('delete.html')
 if __name__ == "__main__":
 	app.run(debug=True)
